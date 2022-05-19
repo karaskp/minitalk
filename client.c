0a3cc3e81aa75fd60c4093adc749daa9c9e75fd3
mcouppe <mcouppe@student.42.fr>
2022-05-19 18:19:42 +0200

    edit files in good folder

--- client.c
+++ client.c
@@ -1,3 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcouppe <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/07 17:00:04 by mcouppe           #+#    #+#             */
+/*   Updated: 2022/04/20 13:44:27 by mcouppe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+/*
+	sigusr1 == 1
+	sigusr2 == 0
+*/
 int received; //see how to handle this
 
 void  ft_sendcbyc(char c, int pid)
@@ -70,3 +88,41 @@ int main(int argc, char **argv)
         sleep (5);
     return (0);
 }
+
+/*
+void	ft_sendback(int pid, char *strtobits)
+{
+	int	i;
+
+	i = 0;
+	if (!strtobits)
+	{
+		ft_printf("Error\nSignal lost\n");
+		exit(EXIT_SUCCESS);
+		return ;
+	}
+	while (strtobits[i] != '\0')
+	{
+		if (strtobits[i] == 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++;
+		usleep(80);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	pid;
+	char	*strtobits;
+	int	i;
+
+	(void)ac;
+	i = 0;
+	pid = ft_atoi(av[1]);
+	strtobits = asciitobinary(av[2]);
+	//send back to server with ft_sendback(pid, strtobits);
+	ft_sendback(pid, strtobits);
+	return (0);
+}*/
