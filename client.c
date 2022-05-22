54cc1ad4dc41b77ce6c870548eef575c4e82f073
marin <m.couppe@hotmail.fr>
2022-05-22 13:21:21 +0200

    starting cleaning

--- client.c
+++ client.c
@@ -6,39 +6,11 @@
 /*   By: mcouppe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 17:00:04 by mcouppe           #+#    #+#             */
-/*   Updated: 2022/05/22 12:36:48 by mcouppe          ###   ########.fr       */
+/*   Updated: 2022/05/22 13:17:19 by mcouppe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-/*
-	sigusr1 == 1
-	sigusr2 == 0
-*/
-/*int main(int argc, char **argv)
-{
-    struct sigaction    sigac;
-
-    received = 1;
-    sigemptyset(&sigac.sa_mask);
-    sigaddset(&sigac.sa_mask, SIGINT);
-    sigaddset(&sigac.sa_mask, SIGQUIT);
-    sigaddset(&sigac.sa_mask, SIGUSR1);
-    sigac.sa_flags = SA_SIGINFO;
-    sigac.sa_sigaction = ft_proto;
-    if (sigaction(SIGUSR2, &sigac, NULL) == -1)
-        ft_error("Error in client sigaction\n");
-    if (ft_atoi(argv[1]) < 0)
-        ft_error("Wrong PID!\n");
-    if (argc == 3)
-        ft_client(ft_atoi(argv[1]), argv[2]);
-    else
-        ft_error("Wrong arguments!\n");
-    while (1)
-        sleep (5);
-    return (0);
-}*/
 
 static void	ft_receive(int signum)
 {
@@ -53,22 +25,6 @@ static void	ft_receive(int signum)
 	}
 }
 
-/*static void	ft_sender_bychar(int pid_serv, unsigned char c)
-{
-	int	i;
-
-	i = 8;
-	while (i--)
-	{
-		if (c >> i & i)
-			kill(pid_serv, SIGUSR2);
-		else
-			kill(pid_serv, SIGUSR1);
-		pause();
-		usleep(200);
-	}
-}
-*/
 static void	ft_sender(int pid_serv, char *str)
 {
 	int	i;
@@ -96,19 +52,6 @@ static void	ft_sender(int pid_serv, char *str)
 	}
 }
 
-/*static int	check_pid(int pid_serv)
-{
-	int	i;
-
-	i = 8;
-	while (i-- && kill(pid_serv, SIGUSR1) == 0)
-	{
-		pause();
-		usleep(200);
-	}
-	return (i == -1);
-}*/
-
 int	main(int ac, char **av)
 {
 	unsigned int	pid_serv;
@@ -121,71 +64,8 @@ int	main(int ac, char **av)
 	pid_serv = ft_atoi(av[1]);
 	signal(SIGUSR1, ft_receive);
 	signal(SIGUSR2, ft_receive);
-/*	if (!check_pid(pid_serv))
-	{
-		ft_printf("Error in pid\n");
-		return (0);
-	}*/
 	ft_sender(pid_serv, av[2]);
 	while (1)
 		pause();
 	return (0);
 }
-
-/*
-
-		USING SIGNAL()///////////////////////////
-
-
-void	ft_sendback(int pid, char c)
-{
-	int	i;
-
-	i = -1;
-	while (++i < 8)
-	{
-		if (c & 0x01)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		c = c >> 1;
-		usleep(100);
-	}
-}
-
-void	ft_sendlen(int pid, int len)
-{
-	int	i;
-
-	i = -1;
-	while (++i < 32)
-	{
-		if (len & 0x01)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		len = len >> 1;
-		usleep(100);
-	}
-}
-
-int	main(int ac, char **av)
-{
-	int		pid;
-	int		i;
-	char	*strtobits;
-
-	if (ac != 3)
-		return (-1);
-	i = -1;
-	pid = ft_atoi(av[1]);
-	if (pid <= 0)
-		return (-1);
-	strtobits = av[2];
-	ft_sendlen(pid, (ft_strlen(strtobits)));
-	while (strtobits[++i])
-		ft_sendback(pid, strtobits[i]);
-	//send back to server with ft_sendback(pid, strtobits);
-	ft_sendback(pid, strtobits[i]);
-	exit(EXIT_SUCCESS);
-}*/
--- server.c
+++ server.c
@@ -6,40 +6,12 @@
 /*   By: mcouppe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 16:59:34 by mcouppe           #+#    #+#             */
-/*   Updated: 2022/05/22 12:37:30 by mcouppe          ###   ########.fr       */
+/*   Updated: 2022/05/22 13:17:50 by mcouppe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-/*
-	sigusr1 == 0
-	sigusr2 == 1
-*/
-/*char	*ft_addonechar(char *old, int bit)
-{
-	int	i;
-	int	size;
-	char	*new;
-
-	i = 0;
-	size = ft_strlen(old);
-	new = malloc(sizeof(char) * size + 2);
-	if (!new)
-		return (NULL);
-	while (old[i] != '\0')
-	{
-		new[i] = old[i];
-		i++;
-	}
-	if (bit == 1)
-		new[i] = '1';
-	else
-		new[i] = '0';
-	new[i + 1] = '\0';
-	free(old);
-	return (new);
-}*/
 static void	ft_catch_signal(int signo, siginfo_t *info, void *context)
 {
 	static unsigned char	c = 0;
@@ -81,84 +53,3 @@ int	main(void)
 		pause();
 	return (0);
 }
-
-/*
-
-		USING SIGNAL////////////////////////////////////////////
-
-
-static void	ft_getlen(int *countbit, char **str, int *len, int signum)
-{
-	static int	len_bis;
-
-	len_bis = 0;
-	if (signum == SIGUSR2)
-		len_bis += ft_converttobit(2, *countbit);
-	if (*countbit == 31)
-	{
-		*len = 1;
-		*str = ft_calloc((len_bis + 1), (sizeof(char)));
-		*countbit = 0;
-		len_bis = 0;
-		return ;
-	}
-	(*countbit)++;
-}
-
-static void	ft_reset(int *len, char **str, int *i)
-{
-	*len = 0; 
-	if (str != NULL)
-	{
-		ft_putendl_fd(*str, 1);
-		free(*str);
-		*str = 0;
-	}
-	*i = 0;
-}
-
-void	ft_server(int signum)
-{
-	static char	*strinbits;
-	static int	charinbit;
-	static int	countbit;
-	static int	len;
-	static int	i;
-
-	countbit = 0;
-	len = 0;
-	i = 0;
-	strinbits = 0;
-	charinbit = 0;
-	if (!len)
-		ft_getlen(&countbit, &strinbits, &len, signum);
-	else
-	{
-		if (signum == SIGUSR2)
-			charinbit += ft_converttobit(2, countbit);
-		if (countbit == 7)
-		{
-			strinbits[i++] = charinbit;
-			countbit = 0;
-			if (charinbit == 0)
-				return (ft_reset(&len, &strinbits, &i));
-			charinbit = 0;
-			return ;
-		}
-		countbit++;
-	}
-}
-
-int	main(void)
-{
-	int			pid_serv;
-	
-
-	pid_serv = (int)getpid();
-	ft_printf("PID server : %d\n", pid_serv);
-	signal(SIGUSR1, ft_server);
-	signal(SIGUSR2, ft_server);
-	while (1)
-		pause();
-	return (0);
-}*/
